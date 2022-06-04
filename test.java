a67c3d96f268eb20d61b740f69230eff67b905ec
ShwetaChavhan <shwetachavhan435@gmail.com>
2022-06-04 14:33:33 +0530

    Change in Hackathin time

--- test.java
+++ test.java
@@ -5,6 +5,6 @@ class main
        System.out.print("Welcome to github :)");
        System.out.print("************Notice************");
        System.out.print("GitHub is arranging Mega Hackathon of 24 hours in Silicon Valley Bengluru");
-       System.out.print("Entery is Free..11th June from Morning 10");
+       System.out.print("Entery is Free..11th June from Morning 11");
    }
 }   
