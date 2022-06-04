d085c541710471807534db58031f6dcc93db81ba
ShwetaChavhan <shwetachavhan435@gmail.com>
2022-06-04 10:42:34 +0530

    Changes to test.java for Git hackathon

--- test.java
+++ test.java
@@ -2,6 +2,9 @@ class main
 {
    public static void main(String []args)
    {
-       System.out.print("Welcome to git :)");
+       System.out.print("Welcome to github :)");
+       System.out.print("************Notice************");
+       System.out.print("GitHub is arranging Mega Hackathon of 24 hours in Silicon Valley Bengluru");
+       System.out.print("Entery is Free..11th June from Morning 10");
    }
 }   
