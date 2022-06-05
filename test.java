19734be2ff88298e232b88d779e82cade46bb4bb
ShwetaChavhan <106465497+ShwetaChavhan@users.noreply.github.com>
2022-06-05 14:23:51 +0530

    Updated test.java to print * pattern

--- test.java
+++ test.java
@@ -3,11 +3,11 @@ class main
    public static void main(String []args)
    {
      int i,j;
-     for(int i=0;i<5;i++)
+     for(int i=1;i<=5;i++)
      {
-       for(int j=0;j<i;j++)
+       for(int j=1;j<=i;j++)
        {
-             System.out.print(j+" ");
+             System.out.print("* ");
        }
        printf("\n");
      }
